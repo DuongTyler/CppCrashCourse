fb9bcad399af5f41bbfdba345ce1a3f8cf075faf
DuongTyler <duongt.sbc@gmail.com>
2019-03-28 14:05:16 -0700

    added 08

---
+++ 08-bin-search.cpp
@@ -0,0 +1,22 @@
+/*
+ * IN THIS LESSON:
+ * 	Exceptions
+ * 	Getting input from the console
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int binSearch(int *arr[], int val)
+{
+	
+}
+
+int main()
+{
+	
+}
