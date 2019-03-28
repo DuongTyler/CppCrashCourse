89bb1614d6a743b7d1221687b5a9f72336db0ac0
DuongTyler <duongt.sbc@gmail.com>
2019-03-28 14:41:52 -0700

    added 08-bin-search

--- 08-bin-search.cpp
+++ 08-bin-search.cpp
@@ -1,7 +1,6 @@
 /*
  * IN THIS LESSON:
- * 	Exceptions
- * 	Getting input from the console
+ * 	An example of how to do a recursive binary search
  */
 
 #include <stdio.h>
@@ -11,12 +10,20 @@
 
 using namespace std;
 
-int binSearch(int *arr[], int val)
+int binSearch(int arr[], int val, int low, int high)
 {
-	
+	int mid = low + (high - low) / 2;
+	if ( arr[mid] > val )
+		return binSearch(arr, val, low, mid-1);
+	else if ( arr[mid] < val )
+		return binSearch(arr, val, mid+1, high);
+	else
+		return mid;
 }
 
 int main()
 {
-	
+	int arr[] = {1,4,7,8,9,15,29,40,47,56,69,82,99,100};
+	int searchfor = 40;
+	printf("Found %d at index %d\n", searchfor, binSearch( arr, searchfor, 0, sizeof(arr)/4));
 }
