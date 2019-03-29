3eb7bb2c8ff66b27e99b95dbadede3adddc77ce8
DuongTyler <duongt.sbc@gmail.com>
2019-03-28 20:25:23 -0700

    added some more user interaction within 08

--- 08-bin-search.cpp
+++ 08-bin-search.cpp
@@ -13,18 +13,21 @@ using namespace std;
 int binSearch(int arr[], int val, int low, int high)
 {
 	int mid = low + (high - low) / 2;
-	if ( arr[mid] > val )
-		return binSearch(arr, val, low, mid-1);
-	else if ( arr[mid] < val )
-		return binSearch(arr, val, mid+1, high);
-	else
-		return mid;
+	if (low > high)			return -1;
+	if ( arr[mid] > val )		return binSearch(arr, val, low, mid-1);
+	if ( arr[mid] < val )		return binSearch(arr, val, mid+1, high);
+	else				return mid;
 }
 
 int main()
 {
 	int arr[] = {1,4,7,8,9,15,29,40,47,56,69,82,99,100};
-	int searchfor = 40;
+	int searchfor;
+	printf("{1,4,7,8,9,15,29,40,47,56,69,82,99,100};\nEnter a number to search: ");
 	cin >> searchfor;
-	printf("Found %d at index %d\n", searchfor, binSearch( arr, searchfor, 0, sizeof(arr)/4));
+	int res = binSearch( arr, searchfor, 0, sizeof(arr)/4);
+	if (res != -1)	
+		printf("Found %d at index %d\n", searchfor, res);
+	else
+		printf("Could not find value %d\n", searchfor);
 }
